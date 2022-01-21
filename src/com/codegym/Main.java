7130cc172631ef4c2bf939c66a0b61649b2ecbb7
lamthuy305 <lamthuy305@gmail.com>
2022-01-21 16:08:46 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,53 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+
+        System.out.println("\nBegin sort processing...");
+        selectionSortByStep(list);
+    }
+
+    public static void selectionSortByStep(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+
+            int currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+                System.out.println("Swap " + list[i] + " with " + list[currentMinIndex]);
+
+            }
+
+            System.out.print("List after the  " + i + "' sort: ");
+            for (int j = 0; j < list.length; j++) {
+                System.out.print(list[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
